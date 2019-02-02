18ac9f2f1bdaa46717df46c66fa79443e2403703
rayansalah <47216479+rayansalah@users.noreply.github.com>
2019-02-02 10:51:30 +0400

    coupoun discount

--- src/main/java/com/hw/coffeeshop/utils/DiscountCalculator.java
+++ src/main/java/com/hw/coffeeshop/utils/DiscountCalculator.java
@@ -2,12 +2,100 @@ package com.hw.coffeeshop.utils;
 
 public class DiscountCalculator {
 	
+	private String foodCatg;
+	private String drinkCatg;
+	private String otherCatg;
+	/*
 	private Float discount1;
 	private Float discount2;
 	private Float totalIncome;
 	private Integer discount1Count;
-	private Integer discount2Count;
-
+	private Integer discount2Count;*/
+	 /** constructoer
+		 * @param f food catg
+		 * @param d drinkCatg
+		 * @param o othercatg
+		
+		 */
+		  public DiscountCalculator(String f, String d, String o ) {
+			   
+			  foodCatg =f;
+			  drinkCatg= d;
+			  otherCatg=o; 
+		  }
+		  
+		  /** check if the first 3 char in the coupon is a number
+		   * @return true or false
+		   */
+		 public boolean firstNumber(char ch) { 
+			  
+		  
+			  return ch >='0' && ch <= '9';
+			  }
+		  
+		  
+		  /** check if the last char in the coupon is a upper case letter
+		   * @return true or false
+		   */
+		  
+		  public boolean isULetter(char ch)
+		  
+		  { 
+			  return ch >= 'A' && ch <= 'Z' ; }
+		  
+		  /*
+		   * 	first it check if the length =5
+		   * then it check if the first 2 char are int
+		   * lastly it check if the last 2 char are uppercase char
+		   */
+		  public boolean validCoupoun(String p) { 
+			  int size = p.length();
+			  if (size !=5)
+			  return false; 
+			  
+			  for (int i=0; i<3; i++) {
+				 char ch = p.charAt(i); 
+				  if(!firstNumber(ch)) {
+					  return false;
+				  }
+			  }
+			  
+			  
+			  for (int i=3; i<size; i++) {
+				 char ch = p.charAt(i); 
+				  if(!isULetter(ch)) {
+					  return false;
+				  }
+			  }
+			
+			  
+				  return true;}
+		  
+		  /*
+		   * it check if the total amount of the order > 200
+		   */
+		  public boolean checkOrderAmount(double m)
+		  {
+			  
+				  
+				  return m> 200;
+			  
+		  }
+		  
+		  /*
+		   * first check if itis valid coupon and the order is >200
+		   * if that true the new toatl amount - 20
+		   * else there is no discount so return the same amount 
+		   */
+		  public Double calcDis1 (String coup, double amount) {
+			  
+			  if(validCoupoun(coup) == true && checkOrderAmount(amount)== true) {
+				  
+				  return amount - 20;}
+			  return amount;
+					  
+		  }
+/*
 	//TODO: implement logic to calculate discount1
 	public Float calculateDiscount1(String orderID,String totalBill){
 		return new Float(discount1);
@@ -21,5 +109,6 @@ public class DiscountCalculator {
 	//TODO: implement logic to calculate total income
 	public Float calculateTotalIncome(){
 		return new Float(totalIncome);
-	}
+	}*/
 }
+
