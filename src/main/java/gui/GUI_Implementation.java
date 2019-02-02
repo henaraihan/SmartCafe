a77e7fbd54030ef0b1d1176eeead83c70c192974
aishaaijazahmad <aishaaijazahmad@gmail.com>
2019-02-03 01:03:06 +0530

    Aisha's second draft

--- src/main/java/gui/GUI_Implementation.java
+++ src/main/java/gui/GUI_Implementation.java
@@ -9,6 +9,8 @@ import java.util.Calendar;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 
 public class GUI_Implementation extends JFrame implements ActionListener
 {
@@ -22,16 +24,19 @@ public class GUI_Implementation extends JFrame implements ActionListener
 		setupWelcomeLabel();
 		setupCategorySelect();
 		setupRadioButtons();
+		setupCurrentOrder();
+		setupButtonRow();
+		
+		mainFrame.pack();
 	}
 	
 	private void setupMainFrame()
 	{
-		mainFrame.setTitle("Coffee Shop simulator");
-		mainFrame.setSize(300, 300);
+		mainFrame.setTitle("Coffee Shop Simulator");
+		//mainFrame.setSize(400, 200);
 		mainFrame.setLocation(600, 200);
-		mainFrame.setLayout(new GridLayout(3, 1, 10, 10));
+		mainFrame.setLayout(new GridLayout(5, 1));
 		mainFrame.setVisible(true);
-		//mainFrame.pack();
 	}
 	
 	private void setupWelcomeLabel()
@@ -39,9 +44,9 @@ public class GUI_Implementation extends JFrame implements ActionListener
 		JPanel forWelcome = new JPanel();
 		JLabel welcomeLabel = new JLabel("Welcome to our Coffee Shop!");
 		forWelcome.add(welcomeLabel);
-		forWelcome.setBorder(new EmptyBorder(10, 10, 10, 10));
+		//forWelcome.setBorder(new EmptyBorder(0, 10, 0, 10));
 		
-		mainFrame.add(welcomeLabel);
+		mainFrame.add(forWelcome);
 	}
 	
 	private void setupCategorySelect()
@@ -49,7 +54,7 @@ public class GUI_Implementation extends JFrame implements ActionListener
 		JPanel forCategory = new JPanel();
 		JLabel categorySelectionLabel = new JLabel("Please Select a category:");
 		forCategory.add(categorySelectionLabel);
-		forCategory.setBorder(new EmptyBorder(10, 10, 10, 10));
+		//forCategory.setBorder(new EmptyBorder(0, 10, 0, 10));
 		mainFrame.add(forCategory); 
 	}
 	
@@ -60,25 +65,73 @@ public class GUI_Implementation extends JFrame implements ActionListener
 		JRadioButton otherCategorySelect = new JRadioButton("Other");
 		
 		JPanel forRadio = new JPanel();
+		//forRadio.setLayout(new GridLayout(1, 3, 10, 10));
+		
+		ButtonGroup gp = new ButtonGroup();
+		gp.add(foodCategorySelect);
+		gp.add(beveragesCategorySelect);
+		gp.add(otherCategorySelect);
+		
 		forRadio.add(foodCategorySelect);
 		forRadio.add(beveragesCategorySelect);
 		forRadio.add(otherCategorySelect);
 		
 		foodCategorySelect.setSelected(true);
 		
-		forRadio.setBorder(new EmptyBorder(10,10,10,10));
+		//forRadio.setBorder(new EmptyBorder(0,10,0,10));
 		
-		//ButtonGroup radioGroup = new ButtonGroup();
-	    //radioGroup.add(foodCategorySelect);
-	    //radioGroup.add(beveragesCategorySelect);
-	    //radioGroup.add(otherCategorySelect);
-	    
-	    //mainFrame.add(foodCategorySelect);
-	    //mainFrame.add(beveragesCategorySelect);
-	    //mainFrame.add(otherCategorySelect);
-	    
 		mainFrame.add(forRadio);
+	}
+	
+	private void setupCurrentOrder()
+	{
+		String[] column = {"OrderID", "Item", "Price"};
+		String[][] orderData = { 	{"100", "Burger", "5"},
+									{"101", "Egg Muffin", "4"},
+									{"102", "Samosa", "2"},
+									{"103", "Chocolate Cupcake", "4"}
+							   };
+		JTable orderTable = new JTable(orderData, column);
+		orderTable.setCellSelectionEnabled(true);
+		//orderTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		resizeColumnWidth(orderTable);
+		JPanel forOrderTable = new JPanel();
+		//forOrderTable.setBorder(new EmptyBorder(0, 5, 0, 5));
+		forOrderTable.add(new JScrollPane(orderTable));
+		mainFrame.add(forOrderTable);		
+	}
+	
+	private void resizeColumnWidth(JTable orderTable) 
+	{
+	    final TableColumnModel columnModel = orderTable.getColumnModel();
+	    for (int column = 0; column < orderTable.getColumnCount(); column++) 
+	    {
+	        int width = 10; // Minimum width
+	        for (int row = 0; row < orderTable.getRowCount(); row++) 
+	        {
+	            TableCellRenderer renderer = orderTable.getCellRenderer(row, column);
+	            Component comp = orderTable.prepareRenderer(renderer, row, column);
+	            width = Math.max(comp.getPreferredSize().width +1 , width);
+	        }
+	        if(width > 300)
+	            width=300;
+	        columnModel.getColumn(column).setPreferredWidth(width);
+	    }
+	}
+	
+	private void setupButtonRow()
+	{
+		JButton remove = new JButton("Remove");
+		JButton select = new JButton("Select");
+		JButton confirmOrder = new JButton("Confirm Order");
 		
+		JPanel buttonRow = new JPanel();
+		buttonRow.setLayout(new GridLayout(1, 3, 5,5));
+		buttonRow.add(remove);
+		buttonRow.add(select);
+		buttonRow.add(confirmOrder);
+		//buttonRow.setBorder(new EmptyBorder(0, 10, 10, 10));
+		mainFrame.add(buttonRow);
 	}
 	
 	
