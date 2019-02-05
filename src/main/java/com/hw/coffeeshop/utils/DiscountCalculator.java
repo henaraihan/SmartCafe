9460942402725b8357219de27642d6bd3d7fa003
Hena Raihan <hena.engg@gmail.com>
2019-02-05 22:35:05 +0400

    fix coupon validation of Discount 1 , Calculate Discount 2, method to save new order data in existing order data

--- src/main/java/com/hw/coffeeshop/utils/DiscountCalculator.java
+++ src/main/java/com/hw/coffeeshop/utils/DiscountCalculator.java
@@ -1,5 +1,11 @@
 package com.hw.coffeeshop.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
 public class DiscountCalculator {
 	
 	private String foodCatg;
@@ -23,6 +29,8 @@ public class DiscountCalculator {
 			  drinkCatg= d;
 			  otherCatg=o; 
 		  }
+		  
+		  public DiscountCalculator() {}
 		  
 		  /** check if the first 3 char in the coupon is a number
 		   * @return true or false
@@ -53,7 +61,7 @@ public class DiscountCalculator {
 			  if (size !=5)
 			  return false; 
 			  
-			  for (int i=0; i<3; i++) {
+			  for (int i=0; i<2; i++) {
 				 char ch = p.charAt(i); 
 				  if(!firstNumber(ch)) {
 					  return false;
@@ -89,12 +97,61 @@ public class DiscountCalculator {
 		   */
 		  public Double calcDis1 (String coup, double amount) {
 			  
-			  if(validCoupoun(coup) == true && checkOrderAmount(amount)== true) {
+			  if(validCoupoun(coup) && checkOrderAmount(amount)) {
 				  
-				  return amount - 20;}
+				  return amount - 20;
+			  }
 			  return amount;
 					  
 		  }
+		  
+		  
+		  public Double calcDisc2(double amount, String customerId, TreeMap<Integer, LinkedList<String>> newCustomerOrder , HashMap<String, ArrayList<String>> newCustomerOrdersMap) {
+			  
+			  if(isDiscount2Applicable(customerId,newCustomerOrder,newCustomerOrdersMap)) {
+			  
+				  return amount * 0.9;
+			  }
+			  return amount;
+		  }
+		  
+		  public Boolean isDiscount2Applicable(String customerId, TreeMap<Integer, LinkedList<String>> newCustomerOrder , HashMap<String, ArrayList<String>> newCustomerOrdersMap) {
+				//if number of order is not equal to 3 then don't apply discount
+				if((newCustomerOrdersMap.get(customerId).size() < 3)) {
+					System.out.println("number of order is not 3");
+					return false;
+				}
+				else
+				{
+					HashSet<String> uniqueCategory = new HashSet<String>();
+					for(String custOrder: newCustomerOrdersMap.get(customerId)) {
+						
+						LinkedList<String>  custOrderListValues = newCustomerOrder.get(Integer.parseInt(custOrder));
+						String category = MenuFileOperations.menuItemsHashMap.get(custOrderListValues.get(1)).get(0);
+						
+						System.out.println(category);
+						uniqueCategory.add(category);
+						
+					}
+					if(uniqueCategory.contains("Beverages")  && uniqueCategory.contains("Other") && uniqueCategory.contains("Food")) {
+						System.out.println("Äll three categories are present");
+						return true;
+					}
+					else
+					{
+						System.out.println("Not all three categories are present");
+						return false;
+					}
+					
+				}
+				
+				
+				
+			}
+		  
+		  
+		  
+		  
 /*
 	//TODO: implement logic to calculate discount1
 	public Float calculateDiscount1(String orderID,String totalBill){
--- src/main/java/com/hw/coffeeshop/utils/ExistingOrderOperations.java
+++ src/main/java/com/hw/coffeeshop/utils/ExistingOrderOperations.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +15,8 @@ import com.hw.coffeeshop.model.Order;
 
 public class ExistingOrderOperations {
 
-	TreeMap<Integer, LinkedList<String>> existingCustomerOrder;
+	public static TreeMap<Integer, LinkedList<String>> existingCustomerOrder;
+	//HashMap<String, ArrayList<String>> customerOrdersMap = new HashMap<String, ArrayList<String>>();
 	public Integer lastOrderNo;
 	private static final String COMMA_DELIMITER = ",";
 	public  void readCSVAndStoreData(){
@@ -43,12 +46,27 @@ public class ExistingOrderOperations {
 	                if(eachOrder.length > 0 )
 	                {
 	                    //Save the order item details in Linked List object
-	                	orderDetails.add(eachOrder[1]);
-	                	orderDetails.add(eachOrder[2]);
-	                	orderDetails.add(eachOrder[3]);
+	                	orderDetails.add(eachOrder[1]); //customerID
+	                	orderDetails.add(eachOrder[2]); //itemID
+	                	orderDetails.add(eachOrder[3]); //quantity 
 	                	
 	                	//populate order in TreeMap with orderID as Key
 	                	existingCustomerOrder.put(Integer.valueOf(eachOrder[0]),orderDetails); 
+	                	
+	                	//Populate CustomerOrderMap
+	                	/*if(customerOrdersMap != null && customerOrdersMap.containsKey(eachOrder[1])) {
+	                		ArrayList<String> orderList = customerOrdersMap.get(eachOrder[1]);
+	                		orderList.add(eachOrder[0]);
+	                		customerOrdersMap.put(eachOrder[1], orderList);
+	                	}
+	                	else
+	                	{
+	                		//for first time
+	                		ArrayList<String> orderList = new ArrayList<String>();
+	                		orderList.add(eachOrder[0]);
+	                		customerOrdersMap.put(eachOrder[1],orderList);
+	                	}*/
+	                	
 	                }
 	            }
 	            // Just to print the TreeMap 
@@ -60,6 +78,16 @@ public class ExistingOrderOperations {
 	    		    System.out.println(" "+MenuFileOperations.menuItemsHashMap.get(entry.getValue().get(1)).toString());
 	    		    
 	    		}
+	            
+	            // Just to print the Customer Orders
+	    		//System.out.println(" Print the Customer Orders List  ");
+	    		/*for (Map.Entry<String, ArrayList<String>>  custOrderList : customerOrdersMap.entrySet()) {
+	    			
+	    			System.out.println(custOrderList.getKey() + ":" + custOrderList.getValue());
+	    			
+	    		}*/
+	            
+	            
 	            
 	    }
         catch(Exception ee)
@@ -79,6 +107,53 @@ public class ExistingOrderOperations {
             }
         }
 	}
+	
+	//Save new Orders data in existing orders
+	public void saveNewOrdersInExistingOrders(TreeMap<Integer, LinkedList<String>> newCustomerOrder) {
+		ExistingOrderOperations.existingCustomerOrder.putAll(newCustomerOrder);
+		
+		// Just to print ALL data as of now (TreeMap)  
+        for (Map.Entry<Integer, LinkedList<String>> entry : ExistingOrderOperations.existingCustomerOrder.entrySet()) {
+			
+		    System.out.println(entry.getKey() + ":" + entry.getValue());
+		    
+		    //PRINT LIST OF MENU ITEMS
+		    System.out.println(" "+MenuFileOperations.menuItemsHashMap.get(entry.getValue().get(1)).toString());
+		    
+		}
+	}
+	/*public Boolean isDiscount2Applicable(String customerId) {
+		//if number of order is not equal to 3 then don't apply discount
+		if((customerOrdersMap.get(customerId).size() < 3)) {
+			System.out.println("number of order is not 3");
+			return false;
+		}
+		else
+		{
+			HashSet<String> uniqueCategory = new HashSet<String>();
+			for(String custOrder: customerOrdersMap.get(customerId)) {
+				
+				LinkedList<String>  custOrderListValues = existingCustomerOrder.get(Integer.parseInt(custOrder));
+				String category = MenuFileOperations.menuItemsHashMap.get(custOrderListValues.get(1)).get(0);
+				
+				System.out.println(category);
+				uniqueCategory.add(category);
+				
+			}
+			if(uniqueCategory.contains("Beverages")  && uniqueCategory.contains("Other") && uniqueCategory.contains("Food")) {
+				System.out.println("Äll three categories are present");
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+		
+		
+		
+	}*/
 	public Integer getLastOrderNumber() {
 		lastOrderNo = existingCustomerOrder.lastKey();
 		return lastOrderNo;
