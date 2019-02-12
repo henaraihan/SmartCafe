3d9d1a34f4daef837f89c6056b4902cb003ffe48
henaraihan <35601310+henaraihan@users.noreply.github.com>
2019-02-12 19:35:39 +0400

    Revert "Custome excp"

--- src/main/java/com/hw/coffeeshop/exceptions/DiscountDoesNotApplyException.java
+++ src/main/java/com/hw/coffeeshop/exceptions/DiscountDoesNotApplyException.java
@@ -1,32 +1,5 @@
 package com.hw.coffeeshop.exceptions;
 
-/**
- * The DiscountDoesNotApplyException wraps all checked standard Java exception and enriches them with a custom error code.
- 
- **/
-public class DiscountDoesNotApplyException extends Exception {
-	
-	 
-	private static final long serialVersionUID = 1L;
-	
-	
-
-	public DiscountDoesNotApplyException(String mesg) {
-		super(mesg);
-	}
-		public DiscountDoesNotApplyException(Throwable cause)
-		{
-			super(cause);
-		}
-		
-		public DiscountDoesNotApplyException(String mesg, Throwable cause) {
-			
-			super(mesg, cause);
-	}
-		public DiscountDoesNotApplyException(String mesg, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-			
-			super(mesg, cause, enableSuppression, writableStackTrace);
-		}
+public class DiscountDoesNotApplyException {
 
 }
-
