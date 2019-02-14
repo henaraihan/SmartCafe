20501c22b30ba42c9c06673467d88aa41fe52ba8
jaa6 <jaa6@hw.ac.uk>
2019-02-14 21:35:48 +0400

    updated junit5 test cases for GUI input validation

---
+++ src/test/java/com/hw/cofeeshop/gui/GUI_ImplementationTest.java
@@ -0,0 +1,14 @@
+package com.hw.cofeeshop.gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GUI_ImplementationTest {
+
+	@Test
+	void test() {
+		// condition here;
+	}
+
+}
