41c09a14464dadc438e0020f50300e53558f0384
jaa6 <jaa6@hw.ac.uk>
2019-02-15 01:43:22 +0400

    updated test cases for Report generator

---
+++ src/test/java/com/hw/coffeeshop/report/TotalIncomeReportGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.hw.coffeeshop.report;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TotalIncomeReportGeneratorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
