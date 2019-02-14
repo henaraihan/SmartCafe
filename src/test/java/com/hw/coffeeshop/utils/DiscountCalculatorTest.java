7e30199dc1d1444f79d8079cfbcebe3fab62f0fa
jaa6 <jaa6@hw.ac.uk>
2019-02-14 20:19:57 +0400

    working_local_copy

---
+++ src/test/java/com/hw/coffeeshop/utils/DiscountCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.hw.coffeeshop.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiscountCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
